bcbc2c222eb99cc9976cf3e06257c8feed2a3bd6
Marcel Stadelmann <marcel@jamon.digital>
2020-04-15 20:38:29 +0200

    removed old site

--- _old/_footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="page-footer">
-    WBG AG – Visuelle Kommunikation<br>Binzstrasse 39, CH-8045 Zürich, +41 44 269 43 43, <a href="mailto:mail@wbg.ch">mail@wbg.ch</a>
-</footer>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhSYFH8MYZDgX7ZDNT7uiRQAu0tU-gpAY"></script>
-<script src="/assets/js/app.js?v=<?php echo time(); ?>"></script>
-<script>MapsUi.init();</script>
-</body>
-</html>
