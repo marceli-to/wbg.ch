23f65344d6e459122907f93ea2b6e646b4ee77e8
Marcel Stadelmann <m@marceli.to>
2022-12-13 13:35:50 +0100

    bf

--- resources/views/web/partials/boxes/teaser-project.blade.php
+++ resources/views/web/partials/boxes/teaser-project.blade.php
@@ -3,5 +3,11 @@
     <img class="lazyload" src="/assets/img/preview.png" data-src="{!! ImageHelper::get($teaser->name, isset($image_size) ? $image_size : 'lg') !!}" height="430" width="280" alt="{{$teaser->caption}}">
   </a>
 @else
-  <img class="lazyload" src="/assets/img/preview.png" data-src="{!! ImageHelper::get($teaser->name, isset($image_size) ? $image_size : 'lg') !!}" height="430" width="280" alt="{{$teaser->caption}}">
+<picture>
+  <source media="(min-width: 1024px)" data-srcset="{!! ImageHelper::get($teaser->name, 'lg') !!}" srcset="/assets/img/preview.png">        
+  <source media="(min-width: 720px)" data-srcset="{!! ImageHelper::get($teaser->name, 'md') !!}" srcset="/assets/img/preview.png">
+  <source data-srcset="{!! ImageHelper::get($teaser->name, 'lg') !!}" srcset="/assets/img/preview.png">
+  <img src="/assets/img/preview.png" data-src="{!! ImageHelper::get($teaser->name, 'sm') !!}" alt="{{$teaser->caption}}" title="{{$teaser->caption}}"  height="430" width="280" class="is-responsive lazy">
+</picture>
+  {{-- <img class="lazyload" src="/assets/img/preview.png" data-src="{!! ImageHelper::get($teaser->name, isset($image_size) ? $image_size : 'lg') !!}" height="430" width="280" alt="{{$teaser->caption}}"> --}}
 @endif
