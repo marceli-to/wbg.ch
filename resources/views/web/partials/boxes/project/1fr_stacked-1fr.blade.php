22d8b829102f16e1c125e113faa59ad2c774d15f
Marcel Stadelmann <m@marceli.to>
2022-12-13 15:16:32 +0100

    bugfix

--- resources/views/web/partials/boxes/project/1fr_stacked-1fr.blade.php
+++ resources/views/web/partials/boxes/project/1fr_stacked-1fr.blade.php
@@ -19,12 +19,12 @@
             @include('web.partials.boxes.article', array('news' => $elements[1]->news))
           @endif
           @if ($elements[1]->image)
-            <picture>
+            {{-- <picture>
               <source media="(min-width: 1024px)" data-srcset="{!! ImageHelper::get($elements[1]->image->name, 'sm') !!}" srcset="/assets/img/preview.png">        
               <source media="(min-width: 720px)" data-srcset="{!! ImageHelper::get($elements[1]->image->name, 'sm') !!}" srcset="/assets/img/preview.png">
               <img src="/assets/img/preview.png" data-src="{!! ImageHelper::get($elements[1]->image->name, 'sm') !!}" alt="{{$elements[1]->image->caption}}" title="{{$elements[1]->image->caption}}"  height="430" width="280" class="is-responsive lazyload">
-            </picture>
-            {{-- @include('web.partials.boxes.teaser-project', array('teaser' => $elements[1]->image, 'image_attribute' => 'height="430" width="280"', 'image_size' => 'md')) --}}
+            </picture> --}}
+            @include('web.partials.boxes.teaser-project', array('teaser' => $elements[1]->image, 'image_attribute' => 'height="430" width="280"', 'image_size' => 'md'))
           @endif
         @endif
       </div>
