b0170331eecab19c28ee74328083714cb7321191
Marcel Stadelmann <m@marceli.to>
2022-12-15 08:56:38 +0100

    staging build

--- resources/views/web/emails/newsletter.blade.php
+++ resources/views/web/emails/newsletter.blade.php
@@ -3,8 +3,9 @@
 <h1>Aufgefrischt: <a href="https://www.wbg.ch" target="_blank" class="highlight">wbg.ch</a></h1>
 <div class="image">
 	<a href="https://www.wbg.ch" target="_blank">
-	  <img src="https://staging.wbg.ch/assets/media/WBGWEB_GIF_2022.gif" width="380" height="512">
+	  <img src="https://www.wbg.ch/assets/media/WBGWEB_GIF_2022.gif" width="380" height="512">
 	</a>
 </div>
+<br>
 <div class="footer">WBG AG<br>Visuelle&nbsp;Kommunikation<br>Binzstrasse&nbsp;39<br>CH-8045&nbsp;Zürich<br>+41&nbsp;44&nbsp;269&nbsp;43&nbsp;43<br><a href="mailto:mail@wbg.ch" style="color: #646464;text-decoration:none">mail@wbg.ch</a></div>
 @endcomponent
