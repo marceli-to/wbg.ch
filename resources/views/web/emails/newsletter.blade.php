213aec3942513f8af66797a6241821c3505eb3a0
Marcel Stadelmann <m@marceli.to>
2022-12-15 08:48:00 +0100

    updates media service, adds gif for newsletter

--- resources/views/web/emails/newsletter.blade.php
+++ resources/views/web/emails/newsletter.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
-<span class="date">13. Dezember 2022</span>
-<h1>Wieder in Ordnung gebracht: <a href="https://www.wbg.ch" target="_blank" class="highlight">wbg.ch</a></h1>
+<span class="date">15. Dezember 2022</span>
+<h1>Aufgefrischt: <a href="https://www.wbg.ch" target="_blank" class="highlight">wbg.ch</a></h1>
 <div class="image">
 	<a href="https://www.wbg.ch" target="_blank">
-	  <img src="http://wbg.ch/assets/media/WBG_Newsletter.gif" width="95" height="128">
+	  <img src="https://staging.wbg.ch/assets/media/WBGWEB_GIF_2022.gif" width="380" height="512">
 	</a>
 </div>
 <div class="footer">WBG AG<br>Visuelle&nbsp;Kommunikation<br>Binzstrasse&nbsp;39<br>CH-8045&nbsp;Zürich<br>+41&nbsp;44&nbsp;269&nbsp;43&nbsp;43<br><a href="mailto:mail@wbg.ch" style="color: #646464;text-decoration:none">mail@wbg.ch</a></div>
