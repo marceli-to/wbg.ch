036db74cb0950cc4a2645e5ae96e44b64ccd5653
Marcel Stadelmann <m@marceli.to>
2022-12-15 11:03:45 +0100

    active newsletter

--- app/Tasks/Newsletter.php
+++ app/Tasks/Newsletter.php
@@ -10,24 +10,24 @@ class Newsletter
     $subscribers = collect($subscribers)->splice(0,1);
     $env = app()->environment();
 
-    // foreach($subscribers->all() as $subscriber)
-    // {
-    //   $recipient = ($env == 'production' && $subscriber->email) ? $subscriber->email : env('MAIL_TO');
-    //   try
-    //   {
-    //     \Mail::to($recipient)->send(
-    //       new \App\Mail\Newsletter()
-    //     );
-    //     $subscriber->processed = 1;
-    //     $subscriber->save();
-    //   }
-    //   catch(\Throwable $e)
-    //   {
-    //     \Log::error($e);
-    //     $subscriber->error = $e;
-    //     $subscriber->processed = 1;
-    //     $subscriber->save();
-    //   }
-    // }
+    foreach($subscribers->all() as $subscriber)
+    {
+      $recipient = ($env == 'production' && $subscriber->email) ? $subscriber->email : env('MAIL_TO');
+      try
+      {
+        \Mail::to($recipient)->send(
+          new \App\Mail\Newsletter()
+        );
+        $subscriber->processed = 1;
+        $subscriber->save();
+      }
+      catch(\Throwable $e)
+      {
+        \Log::error($e);
+        $subscriber->error = $e;
+        $subscriber->processed = 1;
+        $subscriber->save();
+      }
+    }
   }
 }
